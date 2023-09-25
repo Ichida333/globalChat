ad38bd2dab377bbb28f8eb9cfe60a50bf20af505
ichida333 <obama00300@gmail.com>
2023-09-25 15:08:46 +0900

    first

---
+++ backend/config/db.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose")
+
+const connectDB = async() =>{
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+     
+      useUnifiedTopology: true,
+    });
+
+    console.log("ok");
+  } catch (error) {
+    console.log(`error.${error.message}`);
+    process.exit();
+  }
+
+}
+
+module.exports = connectDB
